c480fb6ecf6da0d7699233059e164b814091e663
riza.aycelebi <differentiate.function@gmail.com>
2023-11-15 12:16:28 +0300

    Tema işlemleri tamamlandı.ViewComponentler oluşturuldu.

---
+++ SignalRWebUI/ViewComponents/LayoutComponents/_LayoutScriptsPartialComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.ViewComponents.LayoutComponents
+{
+	public class _LayoutScriptsPartialComponent:ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
