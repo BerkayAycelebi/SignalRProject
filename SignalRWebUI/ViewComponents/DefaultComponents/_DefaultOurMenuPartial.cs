99fc0290e52b5ce5783e00f1a16c780c0941591c
riza.aycelebi <differentiate.function@gmail.com>
2023-12-19 12:01:55 +0300

    UI kısmı tamamlandı.

---
+++ SignalRWebUI/ViewComponents/DefaultComponents/_DefaultOurMenuPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.ViewComponents.DefaultComponents
+{
+    public class _DefaultOurMenuPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
