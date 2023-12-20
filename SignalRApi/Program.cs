4b7506ce04c5c068b72d1e68ebe91df19c471cbd
riza.aycelebi <differentiate.function@gmail.com>
2023-12-20 15:55:40 +0300

    UITasarım sonuna gelindi

--- SignalRApi/Program.cs
+++ SignalRApi/Program.cs
@@ -66,6 +66,9 @@ builder.Services.AddScoped<IMenuTableService, MenuTableManager>();
 builder.Services.AddScoped<IMenuTableDal, EfMenuTableDal>();
 
 
+builder.Services.AddScoped<ISliderService, SliderManager>();
+builder.Services.AddScoped<ISliderDal, EfSliderDal>();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
--- SignalRWebUI/ViewComponents/DefaultComponents/_DefaultSliderComponentPartial.cs
+++ SignalRWebUI/ViewComponents/DefaultComponents/_DefaultSliderComponentPartial.cs
@@ -1,12 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.SliderDtos;
 
 namespace SignalRWebUI.ViewComponents.DefaultComponents
 {
-    public class _DefaultSliderComponentPartial:ViewComponent
+    public class _DefaultSliderComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultSliderComponentPartial(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
+        }
+
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7244/api/Sliders");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultSliderDto>>(jsonData);
+            return View(values);
+
+
+
         }
     }
 }
